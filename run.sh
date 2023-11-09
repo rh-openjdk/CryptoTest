34e998ee82e9f1a91f732e1c0e874b819d913625
Jiri <jvanek@redhat.com>
2023-11-09 17:52:16 +0100

    Fixed bug when jtr files were not compressed if jtreg failed (were test failures)
    And thus lsot the ost precisse thing in worst moment
    Added some minor enhancements to align it with our other runners

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-################################################################################################
+###############################################################################################
+# bash run.sh jdk [bug]
+# bash run.sh jdk [dir]
 # to run without jtreg (without this wrapper), your compilation and runtime on jdk11 and up, will need to be amended:
 # javac -d $dir $clases   --add-reads java.base=ALL-UNNAMED       --add-exports  java.base/com.sun.crypto.provider=ALL-UNNAMED   --add-exports  java.base/sun.security.internal.spec=ALL-UNNAMED     --add-exports  java.base/sun.security.ssl=ALL-UNNAMED  --add-exports  java.base/sun.security.x509=ALL-UNNAMED   --add-reads java.security.jgss=ALL-UNNAMED --add-exports  java.security.jgss/sun.security.jgss=ALL-UNNAMED      --add-exports  java.security.jgss/sun.security.jgss.krb5=ALL-UNNAMED
 # see also acompanying makefile
@@ -15,6 +17,13 @@ while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SOURCE until the file is no longer 
 done
 readonly SCRIPT_DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" && pwd )"
 
+
+envVarArg="-e:CUSTOM_DUMMY_VARIABLE=true,JAVA_TOOL_OPTIONS,OTOOL_BUILD_ARCH,DISPLAY"
+keys=$(env | grep OTOOL_ | sed "s/=.*//")
+for key in $keys; do
+  envVarArg="$envVarArg,$key"
+done
+
 set -e
 set -o pipefail
 
@@ -28,12 +37,16 @@ if [ "x$JAVA_HOME" == "x" ] ; then
   JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
 fi;
 
-TIME=`date +%s`
+TIME=$(date +%s)
 BUGID=${2}
 
-if [ "x$BUGID" != "x" ] ; then 
+FOLDER=${SCRIPT_DIR}
+if [ "x$BUGID" != "x" -a -e "$BUGID" ] ; then
+    FOLDER="$BUGID"
+    BUGID=""
+elif [ "x$BUGID" != "x" ]; then
   BUGID="-bug:$BUGID"
-fi;
+fi
 
 if [ "x$JTREG_HOME" == "x" ] ; then
   JTREG_HOME="$SCRIPT_DIR/jtreg"
@@ -57,6 +70,7 @@ fi
 
 echo Running with $JAVA...
 
+r=0
 mkdir -p test.${TIME}/jdk/JTwork test.${TIME}/jdk/JTreport
 ${JAVA_HOME}/bin/java -jar $JTREG_HOME/lib/jtreg.jar -v1 -a -ignore:quiet \
 		-w:test.${TIME}/jdk/JTwork -r:test.${TIME}/jdk/JTreport \
@@ -64,8 +78,9 @@ ${JAVA_HOME}/bin/java -jar $JTREG_HOME/lib/jtreg.jar -v1 -a -ignore:quiet \
 		-xml \
 		$BUGID \
 		$AGENT_OPT \
-		$SCRIPT_DIR \
-	    | tee test.${TIME}/tests.log
+                $envVarArg \
+		$FOLDER \
+	    | tee test.${TIME}/tests.log || r=$?
 
 tar -czf test.${TIME}.tar.gz test.${TIME}/jdk/JTwork test.${TIME}/jdk/JTreport
 
@@ -77,3 +92,5 @@ fi
 grep -Eqi '^passed:' test.${TIME}/tests.log || exit 1
 # check for failures/errors in tests.log 
 ! grep -Eqi '^(failed|error):' test.${TIME}/tests.log || exit 1
+
+exit $r
